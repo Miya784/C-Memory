0ff79fecd660bf56282c81d5c165b5953088f1e2
burhan <9burhanjehloh@gmail.com>
2024-05-06 17:21:56 +0700

    Big Endian

--- Little_Endian.c
+++ Little_Endian.c
@@ -1,33 +1,44 @@
 #include <stdio.h>
 
-int main() {
-    int i,j,k=0;
-    int MatrixSize=4;
-    int index=16;
-    int numJ[index] ;
-    int numK[16] = {1,2,3,4,5,6,7,8,-8,-7,-6,-5,-4,-3,-2,-1};
+int main()
+{
+    int i, j, k = 0;
+    int MatrixSize = 4;
+    int index = 16;
+    int numJ[index];
+    int numK[16] = {1, 2, 3, 4, 5, 6, 7, 8, -8, -7, -6, -5, -4, -3, -2, -1};
     int numL[index];
 
     printf("numJ : \n");
 
-    for(i=0;i<MatrixSize;i++){
-        for(j=0;j<MatrixSize;j++){
-                scanf("%d",&numJ[k]);
-                k++;
+    for (i = 0; i < MatrixSize; i++)
+    {
+        for (j = 0; j < MatrixSize; j++)
+        {
+            scanf("%d", &numJ[k]);
+            k++;
         }
     }
 
-    k=0;
+    k = 0;
 
-    for(i=0;i<sizeof(numJ)/sizeof(int);i++){
-        numL[i]=numJ[i]*numK[i];
+    for (i = 0; i < sizeof(numJ) / sizeof(int); i++)
+    {
+        numL[i] = numJ[i] * numK[i];
     }
 
     unsigned int *ptr = (unsigned int *)&numL;
 
-    for (i = 0; i < sizeof(numL)/sizeof(int); i++) {
-        printf("Byte %d:Address %p :%d :0x%x\n", i,ptr,*ptr, *ptr);
-        ptr++; 
+    printf("Little Endian\n");
+    for (i = 0; i < sizeof(numL) / sizeof(int); i++)
+    {
+        printf("Byte %d:Address %p :%d :0x%x\n", i, ptr, *ptr, *ptr);
+        ptr++;
+    }
+    printf("Big Endian\n");
+    for (i = sizeof(numL) / sizeof(int) - 1; i >= 0; i--)
+    {
+        printf("Byte %d : Address %p, Value: %d, Hex: 0x%x\n", i, &numL[i], numL[i], numL[i]);
     }
 
     return 0;
